84533a1503ff42b69da5e9b4e28c45de4e14f048
Keshav Dev Kapil <66235005+kapilkeshav@users.noreply.github.com>
2023-01-18 23:34:51 +0530

    Create P2.java

---
+++ Patterns/P2.java
@@ -0,0 +1,20 @@
+/*
+Pattern-2
+******
+*****
+****
+***
+**
+*
+ */
+public class pattern {
+    public static void main(String[] args) {
+        int n = 6;
+        for(int i = n;i>=1;i--){
+            for(int j = 1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
