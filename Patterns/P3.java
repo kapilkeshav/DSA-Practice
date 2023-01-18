8b18a16f60da1deb080fcead2597d66179cf822b
Keshav Dev Kapil <66235005+kapilkeshav@users.noreply.github.com>
2023-01-18 23:47:21 +0530

    Create P3.java

---
+++ Patterns/P3.java
@@ -0,0 +1,23 @@
+/*
+Pattern-3
+******
+ *****
+  ****
+   ***
+    **
+     *
+ */
+public class P3 {
+    public static void main(String[] args) {
+        int n = 6;
+        for(int i = n;i>=1;i--){
+            for(int j = 1;j<=(n-i);j++){
+                System.out.print(" ");
+            }
+            for(int k = 1;k<=i;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
