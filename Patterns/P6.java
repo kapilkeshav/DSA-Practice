933953e3f25cc439813112a4da7262aa6cc42c06
Keshav Dev Kapil <66235005+kapilkeshav@users.noreply.github.com>
2023-01-19 19:29:10 +0530

    Create P6.java

---
+++ Patterns/P6.java
@@ -0,0 +1,44 @@
+/*
+Pattern-6
+       *
+      ***
+     *****
+    *******
+   *********
+  ***********
+ *************
+***************
+       *
+       *
+       *
+       *
+       *
+       *
+    *******
+ */
+public class Pattern{
+    public static void main(String[] args) {
+        int n = 8;
+        for(int i = 1;i<=n;i++){
+            for(int j = 1;j<=(n-i);j++){
+                System.out.print(" ");
+            }
+            for(int k = 1;k<=((2*i)-1);k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int l = 1;l<=n-2;l++){
+            for(int i = 1;i<=(n-1);i++){
+                System.out.print(" ");
+            }
+            System.out.println("*");
+        }
+        for(int i = 4;i<=n-1;i++){
+            System.out.print(" ");
+        }
+        for(int j = 1;j<=((2*4)-1);j++){
+            System.out.print("*");
+        }
+    }
+}
