f0cf3c59828344b34c8d21791f4fff1ff93f426e
Keshav Dev Kapil <66235005+kapilkeshav@users.noreply.github.com>
2023-01-19 19:59:19 +0530

    Create P9.java

---
+++ Patterns/P9.java
@@ -0,0 +1,28 @@
+/*
+Pattern-9
+****************
+*******  *******
+******    ******
+*****      *****
+****        ****
+***          ***
+**            **
+*              *
+ */
+public class Pattern{
+    public static void main(String[] args) {
+        int n = 8;
+        for(int i = n;i>=1;i--){
+            for(int j = 1;j<=i;j++){
+                System.out.print("*");
+            }
+            for(int k = 1;k<=2*(n-i);k++){
+                System.out.print(" ");
+            }
+            for(int l = 1;l<=i;l++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
